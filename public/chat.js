265ff87380fa21636216a30afd1b16bad947c159
navin.kiit <navin.kiit.nk@gmail.com>
2019-10-17 13:42:56 +0530

    okey

--- public/chat.js
+++ public/chat.js
@@ -1,43 +1,48 @@
-$(function () {
-    //make connection
-    var socket = io.connect('https://genexchatapp.herokuapp.com/')
+$(function() {
+  //make connection
+  var socket = io.connect("https://genexchatapp.herokuapp.com/");
 
-    //buttons and inputs
-    var message = $("#message")
-    var username = $("#username")
-    var send_message = $("#send_message")
-    var send_username = $("#send_username")
-    var chatroom = $("#chatroom")
-    var feedback = $("#feedback")
+  //buttons and inputs
+  var message = $("#message");
+  var username = $("#username");
+  var send_message = $("#send_message");
+  var send_username = $("#send_username");
+  var chatroom = $("#chatroom");
+  var feedback = $("#feedback");
 
-    //Emit message
-    send_message.click(function () {
-        socket.emit('new_message', {
-            message: message.val()
-        })
-    })
+  //Emit message
+  send_message.click(function() {
+    socket.emit("new_message", {
+      message: message.val()
+    });
+  });
 
-    //Listen on new_message
-    socket.on("new_message", (data) => {
-        feedback.html('');
-        message.val('');
-        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-    })
+  //Listen on new_message
+  socket.on("new_message", data => {
+    feedback.html("");
+    message.val("");
+    console.log(data.message);
+    chatroom.append(
+      "<p class='message'>" + data.username + ": " + data.message + "</p>"
+    );
+  });
 
-    //Emit a username
-    send_username.click(function () {
-        socket.emit('change_username', {
-            username: username.val()
-        })
-    })
+  //Emit a username
+  send_username.click(function() {
+    socket.emit("change_username", {
+      username: username.val()
+    });
+  });
 
-    //Emit typing
-    message.bind("keypress", () => {
-        socket.emit('typing')
-    })
+  //Emit typing
+  message.bind("keypress", () => {
+    socket.emit("typing");
+  });
 
-    //Listen on typing
-    socket.on('typing', (data) => {
-        feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-    })
+  //Listen on typing
+  socket.on("typing", data => {
+    feedback.html(
+      "<p><i>" + data.username + " is typing a message..." + "</i></p>"
+    );
+  });
 });
